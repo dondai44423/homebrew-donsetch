a8674142c143f98e147582f006b6b6892d154860
dondai44423 <bhandaribishesh879@gmail.com>
2026-09-06 17:32:00 +0545

    donsetch 3.6.4

--- Formula/donsetch.rb
+++ Formula/donsetch.rb
@@ -4,26 +4,26 @@
 class Donsetch < Formula
   desc "Web fetch, search and crawl for AI agents: stealthy, keyless, token-efficient MCP server"
   homepage "https://github.com/dondai44423/donsetch"
-  version "3.6.3"
+  version "3.6.4"
   license "AGPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.3/donsetch-darwin-x64.tar.gz"
-      sha256 "a5d830de6bd778e49f546278282e28cfb4993bfbdf589b23d4a79c085e10942a"
+      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-darwin-x64.tar.gz"
+      sha256 "bbded8f4941623b94a5cc065762b57543d62d288e0d7696984a0e9411c9f4a81"
     else
-      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.3/donsetch-darwin-arm64.tar.gz"
-      sha256 "b913db0e211c32c2d7803a9032371f10fc078cf94990977ac04dbdc74b375201"
+      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-darwin-arm64.tar.gz"
+      sha256 "1652582f27ddcbe053fa204b1a2c2c60b3ecbf9e870eddca317ccc4eb54773e4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.3/donsetch-linux-x64.tar.gz"
-      sha256 "6fcea3ac067d860d6af8143703052ad5fec5037d4661ef7715746b6a5c89fec6"
+      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-linux-x64.tar.gz"
+      sha256 "d55ae035828bcfbeb6b2e8c2456103478d44e26b332d85b3408e3cad8b37d3c6"
     else
-      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.3/donsetch-linux-arm64.tar.gz"
-      sha256 "ba3d504375520c6ae528fbc3ef8239b1ce05c7a04b6205ee201587cc29f242cb"
+      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-linux-arm64.tar.gz"
+      sha256 "4c88c42c35f83f2e9fae57f6fe9db36028635f7a1436112fc3c69b4651f1c0e1"
     end
   end
 
