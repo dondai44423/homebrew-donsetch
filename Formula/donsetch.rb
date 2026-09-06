98afe7f2ef6ac9e6ec0f34a713c5f2927bdf4acf
dondai44423 <bhandaribishesh879@gmail.com>
2026-09-06 22:18:37 +0545

    donsetch 3.6.5: fix slot pairing (hashes re-verified live)

--- Formula/donsetch.rb
+++ Formula/donsetch.rb
@@ -9,21 +9,21 @@ class Donsetch < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-darwin-x64.tar.gz"
-      sha256 "3c282326e96e0104e17a7ff2d3ed88fa436388c46f259a49d0722df733bb32b0"
+      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.5/donsetch-darwin-x64.tar.gz"
+      sha256 "b89512ac761a0936eaa7827d8880120f5c78dab1afd08d1d1c4212c2d9097e50"
     else
-      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-darwin-arm64.tar.gz"
-      sha256 "1652582f27ddcbe053fa204b1a2c2c60b3ecbf9e870eddca317ccc4eb54773e4"
+      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.5/donsetch-darwin-arm64.tar.gz"
+      sha256 "608693bc140ac9b40f1e96a05fda941e69a65949280502582bf4b12b8d62647a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-linux-x64.tar.gz"
-      sha256 "d55ae035828bcfbeb6b2e8c2456103478d44e26b332d85b3408e3cad8b37d3c6"
+      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.5/donsetch-linux-x64.tar.gz"
+      sha256 "5196f9a73fe66c1f3715f03d33ddf0860d6f7d97c2bcbb3e48ea31991bfd4fc4"
     else
-      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-linux-arm64.tar.gz"
-      sha256 "4c88c42c35f83f2e9fae57f6fe9db36028635f7a1436112fc3c69b4651f1c0e1"
+      url "https://github.com/dondai44423/donsetch/releases/download/v3.6.5/donsetch-linux-arm64.tar.gz"
+      sha256 "3c282326e96e0104e17a7ff2d3ed88fa436388c46f259a49d0722df733bb32b0"
     end
   end
 
