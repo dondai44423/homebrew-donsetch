8ddc95d13fe1ea6e127a90d57e69eb200eb23d74
dondai44423 <bhandaribishesh879@gmail.com>
2026-09-06 22:17:59 +0545

    donsetch 3.6.5

--- Formula/donsetch.rb
+++ Formula/donsetch.rb
@@ -4,13 +4,13 @@
 class Donsetch < Formula
   desc "Web fetch, search and crawl for AI agents: stealthy, keyless, token-efficient MCP server"
   homepage "https://github.com/dondai44423/donsetch"
-  version "3.6.4"
+  version "3.6.5"
   license "AGPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-darwin-x64.tar.gz"
-      sha256 "bbded8f4941623b94a5cc065762b57543d62d288e0d7696984a0e9411c9f4a81"
+      sha256 "3c282326e96e0104e17a7ff2d3ed88fa436388c46f259a49d0722df733bb32b0"
     else
       url "https://github.com/dondai44423/donsetch/releases/download/v3.6.4/donsetch-darwin-arm64.tar.gz"
       sha256 "1652582f27ddcbe053fa204b1a2c2c60b3ecbf9e870eddca317ccc4eb54773e4"
